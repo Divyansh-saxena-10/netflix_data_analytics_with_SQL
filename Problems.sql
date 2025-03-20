285227bbe57063598b63f7f22347d1caf59a1fd7
Divyansh <137881440+Divyansh-saxena-10@users.noreply.github.com>
2025-03-20 14:07:15 +0530

    Create Problems.sql

---
+++ Problems.sql
@@ -0,0 +1,17 @@
+//These are the questions/query Related to the Dataset
+
+1. Movie vs. TV Show Distribution: Determine the total number of movies and TV shows available on Netflix.  
+2. Popular Content Ratings: Identify the most frequently assigned ratings for both movies and TV shows.  
+3. Year-Specific Content Search: Retrieve all movies that premiered in a given year (e.g., 2020).  
+4. Top Content-Producing Countries: Rank the top five countries contributing the most content to Netflix.  
+5. Longest Movie Duration: Find the movie with the highest runtime on Netflix.  
+6. Recent Content Additions: Analyze content added to Netflix in the past five years.  
+7. Director-Specific Productions: Extract all movies and TV shows directed by 'Rajiv Chilaka'.  
+8. TV Shows with Extended Seasons: List TV series that have more than five seasons.  
+9. Genre-Based Content Analysis: Count the number of content pieces belonging to each genre.  
+10. Content Trends in India: Identify the top five years with the highest average content release in India.  
+11. Documentary Collection: Retrieve a list of all movies categorized as documentaries.  
+12. Director-Less Content: Find all movies and TV shows that lack an assigned director.  
+13. Actor’s Decade-Wide Presence: Count how many movies featuring 'Salman Khan' were released in the last ten years.  
+14. Most Featured Actors in Indian Movies: Identify the top ten actors who have starred in the highest number of Indian movies.  
+15. Content Categorization by Violence Indicators: Classify content as 'Good' or 'Bad' based on the presence of keywords like 'kill' and 'violence' in the description, and determine the count for each category.  
